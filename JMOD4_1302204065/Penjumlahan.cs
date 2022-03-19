df9fdc66bd76b491a555d1dbd0dee19a3c772692
anwar <saefulanwar7721@gmail.com>
2022-03-19 11:20:39 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

---
+++ JMOD4_1302204065/Penjumlahan.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JMOD4_1302204065
+{
+    public class ClassMain
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+    internal class Penjumlahan
+    { 
+         public static void JumlahTigaAngka<T>( )
+         {
+         }
+        
+
+        
+    }
+}
+
